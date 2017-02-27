f698436793d0cd9cfd51d7e71f423a7089eb8980
Lily-mac <Learner-Bee@github.com>
2017-02-27 21:35:31 +0800

    add readInputStream()

--- java-core-study/src/com/lily/test/Test.java
+++ java-core-study/src/com/lily/test/Test.java
@@ -1,6 +1,11 @@
 package com.lily.test;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import com.lily.util.FileOperations;
 
@@ -13,21 +18,35 @@ import com.lily.util.FileOperations;
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		
+// 输出文件main方法
 //		String content = Multiply.getMultiplyTable();
 //		String filePath = "/Users/Lily/learn/9 multiply 9 table.txt";
-//		
 //		FileOperations.storeIntoFile(content, filePath);
-//		
 //		System.out.println("OK");
+// 读入文件main方法 		
+//		String filePath="/Users/Lily/learn/9 multiply 9 table.txt";
+//		String content = FileOperations.readTxtFile(filePath);
+//		System.out.print(content);
 		
-		String filePath="/Users/Lily/learn/9 multiply 9 table.txt";
-//		FileOperations.readTxtFile(filePath);
+//下载页面图片main方法
+//		URL url=new URL("http://lym25942608.lofter.com/");
+//		HttpURLConnection con=(HttpURLConnection)url.openConnection(); //构造连接
+//		InputStream inputStream=con.getInputStream();//通过输入流获取图片数据，再调用toByteArray()即可获取数据到byte数组
+//		byte[] getData=FileOperations.readInputStream(inputStream);//获取图片到二进制数据
+//		File file=new File("content.txt");
+//		FileOutputStream ops=new FileOutputStream(file);
+//		ops.write(getData);
+//		ops.flush();
+//		ops.close();
+//		System.out.println("Ok");
 		
-		String content = FileOperations.readTxtFile(filePath);
+	
+		String content = "<img src=\"http://imglf1.nosdn.127.net/img/cEpMODErOUtmemlwSlgrQkVGV1c4TEd5WGF5cTh1ZnRlSUdQUFJPWkx2allxOG9Ed1RPRHBBPT0.jpg?imageView&amp;thumbnail=1680x0&amp;quality=96&amp;stripmeta=0&amp;type=jpg\">";
 		
-		System.out.print(content);
-		
+		System.out.println(content.split("<img src=\"")[1]);
 	}
+
+	
+	
 
 }
--- java-core-study/src/com/lily/util/FileOperations.java
+++ java-core-study/src/com/lily/util/FileOperations.java
@@ -1,10 +1,12 @@
 package com.lily.util;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
@@ -24,6 +26,7 @@ public class FileOperations {
 	 *            文件路径
 	 * @throws IOException
 	 */
+	//将输出结果存到本地方法
 	public static void storeIntoFile(String content, String filePath) throws IOException {
 
 		FileOutputStream fos = new FileOutputStream(filePath);
@@ -31,7 +34,7 @@ public class FileOperations {
 		fos.flush();
 		fos.close();
 	}
-
+    //读入指定文件方法
 	public static String readTxtFile(String filePath) throws IOException {
 
 		File file = new File(filePath);
@@ -54,7 +57,19 @@ public class FileOperations {
 		} else {
 			return "找不到指定文件";
 		}
-
 	}
+	
+	//读入图片
+	public static byte[] readInputStream(InputStream inputStream) throws IOException{
+		byte[] buffer=new byte[1024];
+		int len=0;
+		ByteArrayOutputStream bos=new ByteArrayOutputStream();
+		while((len=inputStream.read(buffer))!=-1){
+			bos.write(buffer,0,len);
+		}
+		bos.close();
+		return bos.toByteArray();
+	}
+
 
 }
