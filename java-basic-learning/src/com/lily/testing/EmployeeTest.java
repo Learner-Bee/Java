bcc74b20485fd0d4cf8e0fa9ba45f6c3c79966d6
Learner-Bee <lilyboo@foxmail.com>
2017-04-21 17:59:16 +0800

    add Em\Pri.java and update java learning.md

---
+++ java-basic-learning/src/com/lily/testing/EmployeeTest.java
@@ -0,0 +1,17 @@
+package com.lily.testing;
+//import java.io.*;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		//创建对象
+		Employee employee=new Employee("lily");
+		//调用对象成员，并传值
+		employee.empAge(18);
+		employee.empDisn("QA");
+		employee.empSalary(10000);
+		employee.printEmployee();
+
+	}
+
+}
