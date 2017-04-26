97b1a43182469cfeb8bfcabb53f881e8c5b2029a
Learner-Bee <lilyboo@foxmail.com>
2017-04-26 18:35:08 +0800

    add xunhuan

---
+++ java-basic-learning/src/com/lily/testing/WhileTest.java
@@ -0,0 +1,14 @@
+package com.lily.testing;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+		int a=10;
+		while(a<20){
+			System.out.println("aµÄÖµÎª:"+a);
+			a++;
+		}
+
+	}
+
+}
