24d2a8844bb42dce9abfb0954f0dbdfdcdf8675b
Learner-Bee <lilyboo@foxmail.com>
2017-05-26 18:09:30 +0800

    add String

---
+++ java-basic-learning/src/com/lily/string/CompareTo.java
@@ -0,0 +1,21 @@
+package com.lily.string;
+
+/**
+ * 利用compareTo（String）和compareToIgnoreCase(String)来比较字符串，并返回字符串第一个字母的ASCII的差值
+ * @author Administrator
+ *
+ */
+public class CompareTo {
+
+	public static void main(String[] args) {
+		String str="hello world";
+		String str1="HELLO WORLD";
+		Object objstr=str;
+		
+		System.out.println(str.compareTo(str1));
+		System.out.println(str.compareToIgnoreCase(str1));
+		System.out.println(str.compareTo(objstr.toString()));
+
+	}
+
+}
