d52665476074a1c4f312bb3b925e96b644175e12
Lily-mac <Learner-Bee@github.com>
2017-05-31 22:37:53 +0800

    modefy insertelemrnt

--- java-basic-learning/src/com/lily/arrays/Arraymerge.java
+++ java-basic-learning/src/com/lily/arrays/Arraymerge.java
@@ -3,26 +3,30 @@ package com.lily.arrays;
 import java.util.Arrays;
 
 /**
- * 数组合并利用System.arraycopy()方法，复制数组     报错？？？？
+ * 数组合并利用System.arraycopy()方法，复制数组    
  * * @author Administrator
  *
  */
 public class Arraymerge {
 
 	public static void main(String[] args) {
+		
 		int[] a={4,3,1};
 		int[] b={7,6,3};
-		Integer[] c=new Integer[a.length+b.length];
+		int[] c=new int[a.length+b.length];//合并后的新数组类型必须和原数组类型相同
 		System.arraycopy(a, 0, c, 0, a.length);
 		System.arraycopy(b, 0, c, a.length, b.length);
-		Arrays.sort(c);
+//		Arrays.sort(c);
 		for(int i=0;i<c.length;i++){
 			System.out.print(c[i]+" ");
 		}
+		System.out.println();
 		
 		//判断数组是否相等
 		System.out.println("数组a和数组b是否相等："+a.equals(b));
-
+		
+		
+		
 	}
 
 }
--- java-basic-learning/src/com/lily/arrays/InsertElement.java
+++ java-basic-learning/src/com/lily/arrays/InsertElement.java
@@ -1,32 +1,55 @@
 package com.lily.arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
-
+/**
+ * 数组插入新元素。用到System.arraycopy()方法
+ * 还实现了array转化成arraylist，并用Collections.reverse()反转输出数组
+ * @author Lily
+ *
+ */
 public class InsertElement {
 
 	public static void main(String[] args) {
-		int[] a={1,3,2,4,5};
+		int[] a={1,3,2,5,4};
 		int[] b=new int[a.length+1];
 		//System.out.println(Arrays.binarySearch(a, 0));
+		//将数组转化成arraylist
+		List<Integer> aList = Arrays.asList(1,3,2,5,4);
+		Collections.reverse(aList);
+		printArray(aList);
 		
 		insertElement(a,b);
-		Arrays.sort(b);
+	    printArray(b);
+		Arrays.sort(b,0,3);
 		printArray(b);
 		
 
 	}
+	//插入元素方法。将数组复制给新数组。并定义插入的元素
 	public static int[] insertElement(int[] array1,int[] array2){
 		System.arraycopy(array1, 0, array2, 0, array1.length);
-		array2[array1.length+1]=0;
+		array2[array1.length]=0;
 		return array2;
 	}
+	//循环遍历输出数组
 	public static void printArray(int[] array){
 		for(int i=0;i<array.length;i++){
-			System.out.println(array[i]);
+			System.out.print(array[i]+" ");
 		}
+		System.out.println();
 		
 	}
-
+	//遍历输出arraylist
+	public static void printArray(List<Integer> array){
+		for(int i=0;i<array.size();i++){
+			System.out.print(array.get(i)+" ");
+		}
+		System.out.println();
+		
+	}
+	
 }
