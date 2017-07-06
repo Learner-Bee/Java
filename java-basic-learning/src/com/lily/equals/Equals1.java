9e6df0c01ee849a0ac1d5436819c431a73d0fa93
Lily-mac <Learner-Bee@github.com>
2017-07-07 00:37:38 +0800

    add md-equals\hashcode

---
+++ java-basic-learning/src/com/lily/equals/Equals1.java
@@ -0,0 +1,14 @@
+package com.lily.equals;
+
+public class Equals1 {
+
+	public static void main(String[] args) {
+		String a=new String("abc");
+		String b="1";
+		int c=1;
+		System.out.println(b.equals(c));
+		//System.out.println();
+		System.out.println(a.equals(b));
+	}
+
+}
