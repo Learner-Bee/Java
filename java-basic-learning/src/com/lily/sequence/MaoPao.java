6ccb6bf4ca4886e7ee3eb32fe186331606c8c2d6
Lily-mac <Learner-Bee@github.com>
2017-07-11 23:18:15 +0800

    改进优化

--- java-basic-learning/src/com/lily/sequence/MaoPao.java
+++ java-basic-learning/src/com/lily/sequence/MaoPao.java
@@ -1,32 +1,31 @@
 package com.lily.sequence;
 
 /**
- * 冒泡排序（将最小的浮到最上面）
- * 原理：先找出最大的，然后
+ * 冒泡排序（将最小的浮到最上面） 原理：先找出最大的，然后
+ * 
  * @author Administrator
  *
  */
 public class MaoPao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a={3,2,9,4,3,1,0};
-		for(int i=0;i<a.length;i++){
-			for(int j=0;j<a.length;j++){
-				if(a[i]<a[j]){
-					int b=a[i];
-					a[i]=a[j];
-					a[j]=b;
+		int[] a = { 3, 2, 9, 4, 3, 1, 0 };
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (a[i] < a[j]) {
+					int b = a[j];
+					a[j] = a[i];
+					a[i] = b;
 				}
-				
-			}
-			for(int x:a){
-				System.out.print(x);
+				for (int x : a) {
+					System.out.print(x);
 				}
 				System.out.println();
+
+			}
 			
+
 		}
-		
 
 	}
 
