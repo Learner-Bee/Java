62027036d091acd6d1c777676dfdcc4c71e4f917
Learner-Bee <lilyboo@foxmail.com>
2017-07-12 18:03:52 +0800

    add quick&insertsort

--- java-basic-learning/src/com/lily/sequence/MaoPao.java
+++ java-basic-learning/src/com/lily/sequence/MaoPao.java
@@ -1,7 +1,9 @@
 package com.lily.sequence;
 
 /**
- * 冒泡排序（将最小的浮到最上面） 原理：先找出最大的，然后
+ * 冒泡排序
+ * 1、从第一个元素开始，逐个跟所有元素进行比较，大于该元素交换位置
+ * 2
  * 
  * @author Administrator
  *
@@ -11,19 +13,19 @@ public class MaoPao {
 	public static void main(String[] args) {
 		int[] a = { 3, 2, 9, 4, 3, 1, 0 };
 		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < i; j++) {
+			for (int j = 0; j < i; j++) { 
+				//较少比较次数，j>i即可，因为j=i时是最大元素比较
 				if (a[i] < a[j]) {
 					int b = a[j];
 					a[j] = a[i];
 					a[i] = b;
 				}
-				for (int x : a) {
-					System.out.print(x);
-				}
-				System.out.println();
 
 			}
-			
+			for (int x : a) {
+				System.out.print(x);
+			}
+			System.out.println();
 
 		}
 
--- java-basic-learning/src/com/lily/sequence/QuickSort.java
+++ java-basic-learning/src/com/lily/sequence/QuickSort.java
@@ -1,38 +1,58 @@
 package com.lily.sequence;
 
+/**
+ * 快速排序
+ * 1、定义数组第一个元素key为基准值，最后元素位置j=a.length,最前一个元素i=0
+ * 2、然后从最后元素开始逐个与key比较，直到元素小于key，进行位置置换
+ * 3、此时从最前元素开始逐个与key比较，知道元素大于key，进行交换
+ * 4、当i!=j时继续循环
+ * 5、当i=j结束循环，继续迭代执行该方法，排序key左右两边的数组sort(a,start,i-1);sort(a,j+1,end)
+ * @author Administrator
+ *
+ */
 public class QuickSort {
 
 	public static void main(String[] args) {
 		int[] a = { 4, 3, 6, 2, 1, 5 };
 		int m = 0, n = a.length - 1;
-		// sort(a, m, n);
-		sort2(a, m, n);
+		 sort(a, m, n);
+		//sort2(a, m, n);
 		for (int x : a) {
 			System.out.print(x + " ");
 		}
 	}
-
+    //sort方法自己思路
 	public static void sort(int[] b, int start, int end) {
+		//递归结束条件，防止下标越界
+		if(start>=end){
+			return;
+		}
 
 		int i = start, j = end;
-		int key = b[0];
+		int key = b[i];
 
 		// 从右向左前进，直到小于基准值，交换值
-		while (i < j && key <= b[j]) {
+		while(i!=j){
+		  while (i < j && key <= b[j]) {
 			j--;
 		}
-
-		b[i] = b[j];
-		b[j] = key;
+        swap(b,i,j);
+		//b[i] = b[j];
+		//b[j] = key;
 
 		// 从左向右前进，直到大于基准值，交换
-		while (i < j && key >= b[i + 1]) {
+		while (i < j && key >= b[i]) {
 			i++;
 		}
-		b[j] = b[i + 1];
-		b[i + 1] = key;
+		swap(b,i,j);
+		//b[j] = b[i];
+		//b[i] = key;
 	}
-
+		sort(b,start,j-1);
+		sort(b,i+1,end);
+		
+	}
+    //sort2方法，Tmk思路
 	public static void sort2(int[] b, int start, int end) {
 
 		// 递归结束条件
