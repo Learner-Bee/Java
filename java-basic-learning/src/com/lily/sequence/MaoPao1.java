ab24f71098166542b4d68b98e55255246f2fc17d
Learner-Bee <lilyboo@foxmail.com>
2017-12-04 18:33:42 +0800

    add BD11-30

---
+++ java-basic-learning/src/com/lily/sequence/MaoPao1.java
@@ -0,0 +1,13 @@
+package com.lily.sequence;
+
+public class MaoPao1 {
+
+	public static void main(String[] args) {
+		
+		int[] a={3, 2, 9, 4, 3, 1, 0};
+		for(int i=0;i<a.length;i++){
+			for(int j=)
+		}
+	}
+
+}
